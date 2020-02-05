7d45838af2954d22e10d5f3ac5ba3da1f2d90c44
‘Juliano <julianoperins@gmail.com>
2020-02-05 17:00:09 -0500

    second commit

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-const search = document.getElementById('search');
-const submit = document.getElementById('submit');
-const random = document.getElementById('random');
-const mealsEl = document.getElementById('meals');
-const resultHeading = document.getElementById('result-heading');
-const single_mealEL = document.getElementById('single-meal');
-const alert_message = document.getElementById('alert');
+const search = document.getElementById('search'),
+  submit = document.getElementById('submit'),
+  random = document.getElementById('random'),
+  mealsEl = document.getElementById('meals'),
+  resultHeading = document.getElementById('result-heading'),
+  single_mealEl = document.getElementById('single-meal');
 
 // Search meal and fetch from API
 function searchMeal(e) {
-    e.preventDefault(); // We don't want to submit to a file
+  e.preventDefault();
 
+  // Clear single meal
+  single_mealEl.innerHTML = '';
 
-    //Clear single meal
-    single_mealEL.innerHTML = '';
+  // Get search term
+  const term = search.value;
 
-    const term = search.value;
-
-
-   // Check for empty
+  // Check for empty
   if (term.trim()) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
       .then(res => res.json())
@@ -49,30 +47,80 @@ function searchMeal(e) {
   }
 }
 
-//Fetch meal by ID
+// Fetch meal by ID
 function getMealById(mealID) {
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
     .then(res => res.json())
     .then(data => {
-        const meal = data.meals[0].strInstructions;
-        mealsEl.innerHTML = meal;
-    })
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
 }
 
-// EVENT LISTENERS
+// Fetch random meal from API
+function getRandomMeal() {
+  // Clear meals and heading
+  mealsEl.innerHTML = '';
+  resultHeading.innerHTML = '';
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then(res => res.json())
+    .then(data => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+  `;
+}
+
+// Event listeners
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', e => {
-    const mealInfo = e.path.find(item => {
-        if(item.classList) {
-            return item.classList.contains('meal-info');
-        } else {
-            return false;
-        }
-    });
-    
-    if(mealInfo) {
-        const mealID = mealInfo.getAttribute('data-mealid');
-        getMealById(mealID);
+  const mealInfo = e.path.find(item => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
     }
+  });
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid');
+    getMealById(mealID);
+  }
 });
